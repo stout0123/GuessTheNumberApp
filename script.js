d7bb88863dbd3e083618ab08da37ea9e3bff0452
stout0123 <kidweilenmann2@gmail.com>
2024-08-10 18:53:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -154,9 +154,3 @@ function redeemCode() {
     }
     document.getElementById('redeemCodeInput').value = ''; // Clear the input
 }
-
-    } else {
-        alert('Invalid code. Please try again.');
-    }
-    document.getElementById('redeemCodeInput').value = ''; // Clear the input
-}
